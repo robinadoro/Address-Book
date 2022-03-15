931d051ee14cf33b8968f6a83b9373d7d7ac5cf4
Robinson Idagiza <robinadoro@gmail.com>
2022-03-16 01:59:04 +0300

    ft:Add button display function

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,10 @@ function Contact(first, last){
     this.firstName=first;
     this.lastName=last;
 }
+Contact.prototype.fullName = function() {
+    return this.firstName + " " + this.lastName;
+  }
+  
 
 // user interface logic
 $ *(document).ready(function(){
@@ -18,3 +22,10 @@ $ *(document).ready(function(){
         $('input#new-last-name').val();
     });
 });
+
+$(".contact").last().click(function() {
+    $("#show-contact").show();
+    $("#show-contact h2").text(newContact.firstName);
+    $(".first-name").text(newContact.firstName);
+    $(".last-name").text(newContact.lastName);
+  }); 
