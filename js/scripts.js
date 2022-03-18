310bc9b03f1e6ef3094658298c70f714d40432cf
Robinson Idagiza <robinadoro@gmail.com>
2022-03-18 20:52:38 +0300

    Add interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,37 @@
 // business logic
-function Contact(first, last){
-    this.firstName=first;
-    this.lastName=last;
-}
-Contact.prototype.fullName = function() {
-    return this.firstName + " " + this.lastName;
+function Contact(first, last) {
+    this.firstName = first;
+    this.lastName = last;
   }
   
-
-// user interface logic
-$ *(document).ready(function(){
-    $ ('form#new-contact').submit(function(event){
-        event.preventDefault();
-
-        let inputtedFirstName=$('input#new-first-name').val();
-        let inputtedLastName=$('input#new-last-name').val();
-
-        let newContact = new Contact(inputtedFirstName, inputtedLastName);
-        $('ul#contacts').append("<li><span class='contact'>" + newContact.firstName +"</span></li>");
-        $('input#new-first-name').val();
-        $('input#new-last-name').val();
+  // user interface logic
+  $(document).ready(function() {
+    $("form#new-contact").submit(function(event) {
+      event.preventDefault();
+  
+      var inputtedFirstName = $("input#new-first-name").val();
+      var inputtedLastName = $("input#new-last-name").val();
+  
+      var newContact = new Contact(inputtedFirstName, inputtedLastName);
+  
+      $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+      
+        
+      $("input#new-first-name").val("");
+      $("input#new-last-name").val("");
     });
-});
+  }); 
 
-$(".contact").last().click(function() {
+  $(".contact").last().click(function() {
     $("#show-contact").show();
     $("#show-contact h2").text(newContact.firstName);
     $(".first-name").text(newContact.firstName);
     $(".last-name").text(newContact.lastName);
-  }); 
+  });
+  
+
+  Contact.prototype.fullName = function() {
+    return this.firstName + " " + this.lastName;
+  }
+  
+  
